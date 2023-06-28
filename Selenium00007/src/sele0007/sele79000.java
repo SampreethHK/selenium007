f4330a7a386ca9c6875b185b08e447c0f312cb93
DELL <DELL@DESKTOP-3OFGCVC>
2023-06-28 12:32:56 +0530

    we want VCT

---
+++ Selenium00007/src/sele0007/sele79000.java
@@ -0,0 +1,12 @@
+package sele0007;
+
+public class sele79000 {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		System.out.println("this is a vresion control tool");
+
+	}
+
+}
